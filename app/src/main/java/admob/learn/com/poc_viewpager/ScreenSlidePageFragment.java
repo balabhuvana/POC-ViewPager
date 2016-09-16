4a39cb9656b37c16addcf0986604453a07b8c1b0
balabhuvana <balan.ssn@gmail.com>
2016-09-16 15:30:18 +0530

    Initial commit

---
+++ app/src/main/java/admob/learn/com/poc_viewpager/ScreenSlidePageFragment.java
@@ -0,0 +1,22 @@
+package admob.learn.com.poc_viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bala on 16/9/16.
+ */
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+
+        return rootView;
+    }
+}
